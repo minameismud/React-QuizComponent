3fb7956f3319fc6871cff6b64c80d0d28ebca3ae
Ross Hileman <rhileman@onecallcm.com>
2019-02-12 10:39:43 -0500

    3: Creating a QuizQuestion Component

---
+++ src/QuizQuestion.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class QuizQuestion extends Component {
+
+    constructor(props){
+        super(props);
+        this.onFieldChange = this.onFieldChange.bind(this);
+    }
+  
+    onFieldChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+    render() {
+        return (
+            <main>
+                <section>
+                    <p>{this.props.quiz_question.instruction_text}</p>
+                </section>
+                <section className="buttons">
+                    <ul>
+                        <li>{this.props.quiz_question.answer_options[0]}</li>
+                    </ul>
+                </section>
+            </main>
+        );
+    }
+}
+
+export default QuizQuestion;
